074c8752a2ae6c000c1e6cfa74f1cce66397f04e
mohammad subkhan <subkhanpinter22@gmail.com>
2023-07-27 15:35:33 +0700

    Initial Commit

---
+++ Soccer Shake/Soccer_ShakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Soccer_ShakeApp.swift
+//  Soccer Shake
+//
+//  Created by masbek mbp-m2 on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Soccer_ShakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
